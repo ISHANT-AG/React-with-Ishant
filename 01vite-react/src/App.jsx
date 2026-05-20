32d695be611908a257001f8fbd4c9a2f56ad8604
Ishant Agarwal <agarwalishant690@gmail.com>
2026-05-20 19:58:21 +0530

    ishant

--- 01vite-react/src/App.jsx
+++ 01vite-react/src/App.jsx
@@ -1,8 +1,11 @@
 
 function App() {
-
+  
   return (
-   <h1>Chai aur React with Vite || Ishant Agrawal</h1>
+    <>
+      <h1>Chai aur React with Vite || Ishant Agrawal</h1>
+      <p> Test Ishant </p>
+    </>
   )
 }
 
